6c20d859dabe2b229b8019f6817485ecc4a009c3
Kyle Schwendiman <kwschwen@gmail.com>
2024-04-14 18:16:36 -0400

    Created the model for the game session

---
+++ SimpleSlotMachine/Models/UserSession.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleSlotMachine.Models
+{
+    public class UserSession
+    {
+        public string SessionId { get; set; }
+        public int StartingCredits { get; set; }
+        public int CurrentCredits { get; set; }
+    }
+}
