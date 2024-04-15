8d69c1707768b03069474e3471da1d563b6544e6
Kyle Schwendiman <kwschwen@gmail.com>
2024-04-14 21:58:21 -0400

    Created the controller to handle the rolls

---
+++ SimpleSlotMachine/Controllers/RollsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleSlotMachine.Services;
+
+namespace SimpleSlotMachine.Controllers
+{
+    [Route("api/rolls/{sessionId}")]
+    [ApiController]
+    public class RollsController : ControllerBase
+    {
+        private readonly ISlotMachineService _slotMachineService;
+        private readonly ISessionService _sessionService;
+
+        public RollsController(ISlotMachineService slotMachineService)
+        {
+            _slotMachineService = slotMachineService;
+        }
+
+        [HttpPost("start")]
+        public IActionResult StartRoll(string sessionId)
+        {
+            var result = _slotMachineService.StartRoll(sessionId);
+            return Ok(result);
+        }
+    }
+}
